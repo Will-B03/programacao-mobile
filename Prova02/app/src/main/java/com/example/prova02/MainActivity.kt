c057bc26d6262f47276d2f1bfd19996511b73198
Willison Bonfim <115009108+Will-B03@users.noreply.github.com>
2024-05-17 20:35:54 -0300

    Add files via upload

---
+++ Prova02/app/src/main/java/com/example/prova02/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.prova02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.prova02.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+    }
+}
